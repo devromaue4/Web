1e421bc4303f9dcaa5a19ff513f5cc9a0f0dd35f
devromaue4 <devroma@hotmail.com>
2015-06-04 18:06:24 +0600

    up

---
+++ MyProjects/WebSite1/js/js-adv.js
@@ -0,0 +1,6 @@
+﻿
+//var point = new Object();
+
+//point.myProperties = 20;
+
+//document.write('Значение свойства point.myProperties: ' + point.myProperties + '<br/>');
---
+++ MyProjects/WebSite1/js/script.js
@@ -0,0 +1,27 @@
+﻿/* 
+    Файл JavaScript 
+*/
+
+//function clicked(e) {
+//    console.log('clicked', e)
+//    //if (e.target.style.background != 'red' )
+//        e.target.style.background = 'red';
+//    //else
+//    //    e.target.style.background = '#ccc';
+//}
+
+//function clicked_out(e) {
+//    console.log('clicked', e)
+//    //if (e.target.style.background != 'red')
+//    //    e.target.style.background = 'red';
+//    //else
+//        e.target.style.background = '#ccc';
+//}
+
+//window.onload = function () {
+//    var content_aaa = document.querySelectorAll('.content');
+//    for (var i = 0; i < content_aaa.length; i++) {
+//        content_aaa[i].onmouseover = clicked;
+//        content_aaa[i].onmouseout = clicked_out;
+//    }
+//}
